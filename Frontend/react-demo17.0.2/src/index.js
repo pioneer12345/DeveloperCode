54ebf6a8a7e12b9e94379b3a1e717eb11f5b8b8d
Chen Ma <ma_chenn@163.com>
2022-10-06 16:16:24 +0800

    test

--- Frontend/react-demo17.0.2/src/index.js
+++ Frontend/react-demo17.0.2/src/index.js
@@ -2,7 +2,7 @@
  * @Author: Chen Ma ma_chenn@163.com
  * @Date: 2022-09-09 13:38:32
  * @LastEditors: Chen Ma ma_chenn@163.com
- * @LastEditTime: 2022-10-06 15:58:47
+ * @LastEditTime: 2022-10-06 16:15:26
  * @FilePath: \react-demo\src\index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -10,25 +10,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {Routes} from "react-router-dom";
 import {Link,Route,HashRouter} from "react-router-dom";
-import Home from "./components/Home";
-import User from "./components/User";
-import Profile from "./components/Profile";
 
 ReactDOM.render(
-  <HashRouter>
-    <>
-    <ul>
-      <li><Link to="/">Home</Link></li>
-      <li><Link to={{pathname:"/user",state:{id:1}}}>User</Link></li>
-      <li><Link to="/profile">Profile</Link></li>
-    </ul>
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/user" element={<User />}/>
-        <Route path="/profile" element={<Profile />}/>
-      </Routes>
-    </>
-  </HashRouter>
+  <input></input>
   ,
   document.getElementById('root')
 );
